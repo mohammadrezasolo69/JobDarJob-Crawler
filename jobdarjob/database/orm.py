07fb3f14007fe4811e4522d0f19912148db85668
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-05-15 19:27:22 +0330

    change method insert

--- jobdarjob/database/orm.py
+++ jobdarjob/database/orm.py
@@ -1,20 +1,23 @@
 import os
-import logging
+# import logging
+import urllib
+from urllib.parse import unquote
+
 import colorlog
 
 from clickhouse_driver import Client
 
-os.system('clear')
-logger = colorlog.getLogger()
+# os.system('clear')
+# logger = colorlog.getLogger()
+#
+# logger.setLevel(logging.INFO)
+# handler = colorlog.StreamHandler()
+# handler.setFormatter(colorlog.ColoredFormatter(
+#     '%(log_color)s%(levelname)s:%(message)s "LINE":%(lineno)d\n'
+#     '------------------------------------------------------------'
+# ))
 
-logger.setLevel(logging.INFO)
-handler = colorlog.StreamHandler()
-handler.setFormatter(colorlog.ColoredFormatter(
-    '%(log_color)s%(levelname)s:%(message)s "LINE":%(lineno)d\n'
-    '------------------------------------------------------------'
-))
-
-logger.addHandler(handler)
+# logger.addHandler(handler)
 
 
 class ClickHouse:
@@ -42,10 +45,11 @@ class ClickHouse:
 
         try:
             self.client = Client(host=host, port=port)
-            logging.info('Connected to ClickHouse')
+            # logging.info('Connected to ClickHouse')
 
         except Exception as e:
-            logging.error(f'Failed to connect to ClickHouse: {e}')
+            pass
+            # logging.error(f'Failed to connect to ClickHouse: {e}')
 
 
 class Database:
@@ -56,10 +60,11 @@ class Database:
     def create(self, database_name: str, using=True):
         try:
             self.client.execute(f'CREATE DATABASE IF NOT EXISTS {database_name};')
-            logging.info("Database " + database_name + " has created.")
+            # logging.info("Database " + database_name + " has created.")
 
         except Exception as e:
-            logging.error(f"Database Creation has failed: {str(e)}")
+            pass
+            # logging.error(f"Database Creation has failed: {str(e)}")
 
         if using:
             self.use(database_name)
@@ -67,19 +72,21 @@ class Database:
     def drop(self, database_name: str):
         try:
             self.client.execute(f'DROP DATABASE IF EXISTS {database_name}')
-            logging.info("Database " + database_name + " has deleted.")
+            # logging.info("Database " + database_name + " has deleted.")
 
         except Exception as e:
-            logging.error(f"Database Delete has failed: {str(e)}")
+            pass
+            # logging.error(f"Database Delete has failed: {str(e)}")
 
     def use(self, database_name: str):
         try:
             self.client.execute(f'USE {database_name};')
             self.active_database = database_name
-            logging.info("Database " + database_name + " has selected.")
+            # logging.info("Database " + database_name + " has selected.")
 
         except Exception as e:
-            logging.error(f"Database select has failed: {str(e)}")
+            pass
+            # logging.error(f"Database select has failed: {str(e)}")
 
     def all(self):
         return self.client.execute(f'SHOW DATABASES;')
@@ -97,19 +104,29 @@ class Database:
                 field += f'{key} {value},'
         query = f"""CREATE TABLE IF NOT EXISTS {table_name} ({field}) ENGINE={engine} {setting};"""
         self.client.execute(query)
-        logging.info(f'Table {table_name} Created.')
+        # logging.info(f'Table {table_name} Created.')
 
     def drop_table(self, table_name: str) -> None:
         query = f"""DROP TABLE IF EXISTS {table_name};"""
         self.client.execute(query)
-        logging.info(f'Table {table_name} Deleted.')
+        # logging.info(f'Table {table_name} Deleted.')
 
     def insert(self, table_name: str, fields: dict):
-        keys, values = zip(*fields.items())
-        query = f'INSERT INTO {table_name} ({",".join(keys)}) VALUES'
-        self.client.execute(query, [list(values)])
+        # columns = ", ".join(fields.keys())
+        # values = ", ".join([f"'{value}'" if value is not None else "NULL" for value in fields.values()])
+        # query = f"INSERT INTO {table_name} ({columns}) VALUES ({values})"
+        # print("******************************************")
+        # print(query)
+        # self.client.execute(query)
+        column_value: list = []
+        for i in fields.values():
+            column_value.append(self._check_encoded(i))
 
-        logging.info('Insert Complete . ')
+        self.client.execute(
+            f"""INSERT INTO {table_name}({','.join(fields.keys())}) values ({','.join(column_value)})"""
+        )
+
+
 
     def select(self, table_name: str, fields: list):
         query = f"""
@@ -131,4 +148,12 @@ class Database:
         query = f'DESCRIBE {table_name}'
         return self.client.execute(query)
 
-
+    @staticmethod
+    def _check_encoded(value: str = 'None') -> str:
+        if value == 'None' or value == None:
+            return 'NULL'
+        elif value != 'NULL' and type(value) == type(str()):
+            value = unquote(value)
+            return f"'{value}'"
+        else:
+            return str(value)

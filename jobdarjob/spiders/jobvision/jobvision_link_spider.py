85e130d77c9878fdfbfe030df720b6596e7b81c7
mohamamdrezasolo <mohamamdrezasolo69@gmail.com>
2023-05-29 23:43:18 +0330

    add package scrapy-playwrite in req.txt

--- jobdarjob/spiders/jobvision/jobvision_link_spider.py
+++ jobdarjob/spiders/jobvision/jobvision_link_spider.py
@@ -1,11 +1,25 @@
+import re
+
 import scrapy
 
 
 class JobvisionLinkSpider(scrapy.Spider):
     name = 'jobvision_link'
-    start_urls = [
-        'https://jobvision.ir/jobs',
-        ]
+    custom_settings = {
+        'DOWNLOAD_TIMEOUT': 0
+    }
+
+    def start_requests(self):
+        yield scrapy.Request('https://jobvision.ir/jobs', callback=self.parse, meta={"playwright": True})
 
     def parse(self, response, **kwargs):
-        ...
+        for item in response.xpath(
+                '/html/body/app-root/div/app-jobs/section/div[2]/div[1]/div/div[1]/div/job-card-list/job-card'):
+
+            link = item.xpath('a/@href').get()
+
+            match = re.match(r"/jobs/(\d+)/", link)
+            if match:
+                company_id = match.group(1)
+
+                yield {'company_id': company_id}

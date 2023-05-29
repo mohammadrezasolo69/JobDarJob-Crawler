c9f2eda24a02af85b78565e3328da7ddfb3fa2da
mohamamdrezasolo <mohamamdrezasolo69@gmail.com>
2023-05-29 18:27:04 +0330

    config scrapy-splash in setting.py

--- jobdarjob/settings.py
+++ jobdarjob/settings.py
@@ -1,8 +1,14 @@
-
+# Config Clickhouse
 CLICKHOUSE_HOST = 'localhost'
 CLICKHOUSE_PORT = 9000
 CLICKHOUSE_DATABASE = 'Jobdarjob'
 
+# Config Splash
+SPLASH_URL = 'http://localhost:8050'
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
+
+# --------------------------------------------------------------------------------------
 BOT_NAME = "jobdarjob"
 
 SPIDER_MODULES = ["jobdarjob.spiders"]
@@ -39,16 +45,17 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-# SPIDER_MIDDLEWARES = {
-#    "jobdarjob.middlewares.JobdarjobSpiderMiddleware": 543,
-# }
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    "jobdarjob.middlewares.JobdarjobDownloaderMiddleware": 543,
-# }
-
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
 # EXTENSIONS = {

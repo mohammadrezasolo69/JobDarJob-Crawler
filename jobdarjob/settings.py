d9004b8322bb02ce12709c5e6b244f24f5df097a
mohamamdrezasolo <mohamamdrezasolo69@gmail.com>
2023-05-29 23:41:58 +0330

    remove config splash and add config playwrite in setting.py

--- jobdarjob/settings.py
+++ jobdarjob/settings.py
@@ -3,12 +3,12 @@ CLICKHOUSE_HOST = 'localhost'
 CLICKHOUSE_PORT = 9000
 CLICKHOUSE_DATABASE = 'Jobdarjob'
 
-# Config Splash
-SPLASH_URL = 'http://localhost:8050'
-DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
-HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
+# Config PlayWrite
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+PLAYWRIGHT_BROWSER_TYPE = "chromium"
 
 # --------------------------------------------------------------------------------------
+
 BOT_NAME = "jobdarjob"
 
 SPIDER_MODULES = ["jobdarjob.spiders"]
@@ -45,16 +45,15 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-SPIDER_MIDDLEWARES = {
-    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
-}
+# SPIDER_MIDDLEWARES = {
+#     
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-DOWNLOADER_MIDDLEWARES = {
-    'scrapy_splash.SplashCookiesMiddleware': 723,
-    'scrapy_splash.SplashMiddleware': 725,
-    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+DOWNLOAD_HANDLERS = {
+    "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+    "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
 }
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html

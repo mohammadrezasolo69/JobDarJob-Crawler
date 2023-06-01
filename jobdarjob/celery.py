01fecd6702a21f75c0279d45b20795d0697f3afa
mohamamdrezasolo <mohamamdrezasolo69@gmail.com>
2023-06-01 11:58:48 +0330

    add task in beat celery

--- jobdarjob/celery.py
+++ jobdarjob/celery.py
@@ -1,4 +1,31 @@
+from datetime import timedelta
+
 from celery import Celery
 
 app = Celery(main='jobdarjob', broker='redis://localhost:6379')
 app.autodiscover_tasks(['jobdarjob.tasks'], force=True)
+
+app.conf.beat_schedule = {
+    'run_jobinja_link_spider': {
+        'task': 'jobdarjob.tasks.run_jobinja_link_spider',
+        'schedule': timedelta(days=1),
+        'args': (),
+    },
+    'run_jobinja_single_spider': {
+        'task': 'jobdarjob.tasks.run_jobinja_single_spider',
+        'schedule': timedelta(days=1),
+        'args': (),
+        'relative': True,
+    },
+    'run_quera_link_spider': {
+        'task': 'jobdarjob.tasks.run_quera_link_spider',
+        'schedule': timedelta(days=1),
+        'args': (),
+    },
+    'run_quera_single_spider': {
+        'task': 'jobdarjob.tasks.run_quera_single_spider',
+        'schedule': timedelta(days=1),
+        'args': (),
+        'relative': True,
+    },
+}
